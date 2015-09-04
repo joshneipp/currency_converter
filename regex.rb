ac3c7386aead6e5a81e97532de2c83429a5f153f
joshneipp <joshneipp@gmail.com>
2015-09-04 19:32:29 -0400

    Raise DifferentCurrencyCodeError

--- regex.rb
+++ regex.rb
@@ -1,37 +1,26 @@
-# puts "Type a currency and amount: "
-# user_input = gets.chomp
-#
-# def isolate_symbols(user_input)
-#   user_input = gets.chomp
-#   return user_input
-# end
-# isolate_symbols
-#
-# var = user_input
-# puts
-
-# def user_input
-#   puts "Number please: "
-#   gets.chomp
-#   return gets.chomp
-# end
-#
 # def isolate_symbols
-#   no_digits = user_input.gsub(/[^\D]/, "")
+#   puts "Number please: "
+#   x = gets.chomp
+#   no_digits = x.gsub(/[^\D]/, "")
 #   no_digits_no_punctuation = no_digits.delete ",."
+#   puts no_digits_no_punctuation
 # end
-#
-# puts no_digits
 
-def isolate_symbols
-  puts "Number please: "
-  x = gets.chomp
-  no_digits = x.gsub(/[^\D]/, "")
-  no_digits_no_punctuation = no_digits.delete ",."
-  puts no_digits_no_punctuation
-end
+#isolate_symbols
 
-isolate_symbols
+array = ["USD",
+         "EUR",
+         "JPY",
+        ]
+
+puts "Gimme code!"
+example = gets.chomp
+happy_answer = "yes"
+sad_answer = "nope!"
+array.any?{ |i| i[example] } ? answer = happy_answer : answer = sad_answer
+puts answer
+
+
 # puts "gimme dollarz: "
 # input = gets.chomp
 # no_digits = input.gsub(/[^\D]/, "")
--- store_currency.rb
+++ store_currency.rb
@@ -1,49 +1,104 @@
+class DifferentCurrencyCodeError < StandardError
+end
+
 class Currency
 
-attr_reader :amount
-attr_reader :code
+  attr_reader :amount, :code_symbol
+  attr_reader :code
+  codes_hash = {}
 
-  def initialize(amount, code)
-    @amount = amount
-    @code   = code
+  def initialize(amount, code = "")
+    codes_hash = {"$" => "USD",
+                  "€" => "EUR",
+                  "¥" => "YEN"}
+
+    if code != ""
+      @amount = amount
+      @code = code
+    else
+      code_symbol = amount.gsub(/[^\D]/, "")
+      code_symbol = code_symbol.gsub(/\./, "")
+      # code_symbol[0..-1]
+      # puts code_symbol
+      # @code = code_symbol[0]
+      puts code_symbol
+      @code = codes_hash[code_symbol].to_s
+      @amount = amount.gsub(/[^\d\.]/, "").to_f
+      puts @amount
+    end
   end
 
   def + (other_money)
-    other_money.amount += @amount
+    if @code != other_money.code
+      raise DifferentCurrencyCodeError
+    end
+    Currency.new(@amount + other_money.amount, @code)
   end
 
   def - (other_money)
-    other_money.amount -= @amount
+    if @code != other_money.code
+      raise DifferentCurrencyCodeError
+    end
+    Currency.new(@amount - other_money.amount, @code)
   end
 
-  def * (other_money)
-    other_money.amount *= @amount
-    return other_money.amount
+  def * (value)
+    Currency.new(@amount * value, @code)
   end
 
   def ==(other_money)
     other_money.amount == @amount && other_money.code == @code
   end
-
 end
 
-dollar       =  Currency.new(100.00, "USD")
+#dollar_check1 = Currency.new("$1.20")
+# dollar_check2 = Currency.new(100 USD)
+# dollar_check2 = (100 USD)
+# euro_check1   = "€7.00"
+# euro_check2   = (100 EUR)
+dollar       =  Currency.new(1000.00, "USD")
 joshmoney    =  Currency.new(100.00, "USD")
 damemoney    =  Currency.new(100.00, "DMZ")
-anthonymoney =  Currency.new(1000, "USD")
+anthonymoney =  Currency.new(1000,   "USD")
 
-def isolate_symbols
-  puts "Number please: "
-  x = gets.chomp
-  no_digits = x.gsub(/[^\D]/, "")
-  no_digits_no_punctuation = no_digits.delete ",."
-  puts no_digits_no_punctuation
-end
+sum = dollar + joshmoney
+puts sum
+puts sum.amount
+puts sum.code
 
-isolate_symbols
 
+product = joshmoney * dollar.amount
+puts product.amount
+puts product
+puts product.code
+
+# puts sum.amount
+# puts sum.code
 
 #
+# def isolate_symbols
+#   puts "Number please: "
+#   x = gets.chomp
+#   no_digits = x.gsub(/[^\D]/, "")
+#   no_digits_no_punctuation = no_digits.delete ",."
+#   no_symbols = x.gsub(/[^\d\.]/, "").to_f
+#   #without_symbol = no_symbol.to_f
+#   puts "Currency symbol is #{no_digits_no_punctuation}"
+#   puts "Currency amount is #{no_symbol}"
+#   # sum = without_symbol += without_symbol
+#   # puts "#{sum}"
+# end
+#
+#
+#
+# isolate_symbols
+#
+# 3.times do
+#   puts "Subtraction"
+#   puts joshmoney.amount - damemoney.amount
+# end
+#
+# #
 # 2.times do
 #   puts "gimme dollrz: "
 #   input = gets.chomp
@@ -85,5 +140,3 @@ isolate_symbols
 # 3.times do
 #   puts "I have #{joshmoney.amount} JoshMoneys!"
 # end
-
-isolate_symbols
